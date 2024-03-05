7db91afc451d288addb285054723ee44c6668d4b
XiaoFei <1903078434@qq.com>
2024-03-05 20:12:27 +0800

    添加方法

--- api-client-sdk/src/main/java/com/xiaofeio/api/autoconfigure/ApiClient.java
+++ api-client-sdk/src/main/java/com/xiaofeio/api/autoconfigure/ApiClient.java
@@ -41,15 +41,100 @@ public class ApiClient {
         // 请求地址
         String requestUrl = apiProperties.getGatewayUrl() + "/interface/region/list/page";
 
+        HttpEntity<String> httpEntity = getHttpEntity(encrypt);
+        // 发送 POST 请求
+        return restTemplate.postForEntity(requestUrl, httpEntity, String.class).getBody();
+
+    }
+
+    /**
+     * 根据传递的参数进行分页查询
+     *
+     * @return 指定页的数据
+     */
+    public String selectRegionById(Long id) {
+        //加密数据
+        String encrypt = EncryptionUtils.encrypt(String.valueOf(id), apiProperties.getSecretKey());
+        // 请求地址
+        String requestUrl = apiProperties.getGatewayUrl() + "/interface/region";
+
+        HttpEntity<String> httpEntity = getHttpEntity(encrypt);
+        // 发送 POST 请求
+        return restTemplate.postForEntity(requestUrl, httpEntity, String.class).getBody();
+
+    }
+
+    /**
+     * 根据传递的参数进行分页查询
+     *
+     * @return 指定页的数据
+     */
+    public String selectRegionOne(Map<String, Object> paramsMap) {
+        //加密数据
+        String encrypt = EncryptionUtils.encrypt(JSONUtil.toJsonStr(paramsMap), apiProperties.getSecretKey());
+        // 请求地址
+        String requestUrl = apiProperties.getGatewayUrl() + "/interface/region/one";
+
+        HttpEntity<String> httpEntity = getHttpEntity(encrypt);
+        // 发送 POST 请求
+        return restTemplate.postForEntity(requestUrl, httpEntity, String.class).getBody();
+
+    }
+
+    /**
+     * 根据传递的参数进行分页查询
+     *
+     * @return 指定页的数据
+     */
+    public String selectRegionList() {
+        // 请求地址
+        String requestUrl = apiProperties.getGatewayUrl() + "/interface/region/list";
+
+        HttpEntity<String> httpEntity = getHttpEntity(null);
+        // 发送 POST 请求
+        return restTemplate.postForEntity(requestUrl, httpEntity, String.class).getBody();
+
+    }
+
+    /**
+     * 根据传递的参数进行分页查询
+     *
+     * @return 指定页的数据
+     */
+    public String selectRegionListTree() {
+        // 请求地址
+        String requestUrl = apiProperties.getGatewayUrl() + "/interface/region/tree";
+
+        HttpEntity<String> httpEntity = getHttpEntity(null);
+        // 发送 POST 请求
+        return restTemplate.postForEntity(requestUrl, httpEntity, String.class).getBody();
+
+    }
+
+    /**
+     * 根据传递的参数进行分页查询
+     *
+     * @return 指定页的数据
+     */
+    public String listRegionBySearch(Map<String, Object> paramsMap) {
+        //加密数据
+        String encrypt = EncryptionUtils.encrypt(JSONUtil.toJsonStr(paramsMap), apiProperties.getSecretKey());
+        // 请求地址
+        String requestUrl = apiProperties.getGatewayUrl() + "/interface/region/list/search";
+
+        HttpEntity<String> httpEntity = getHttpEntity(encrypt);
+        // 发送 POST 请求
+        return restTemplate.postForEntity(requestUrl, httpEntity, String.class).getBody();
+
+    }
+
+    private HttpEntity<String> getHttpEntity(String encrypt) {
         // 请求头
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("accessKey", apiProperties.getAccessKey());
         // 请求体
-        HttpEntity<String> requestEntity = new HttpEntity<>(encrypt, headers);
-        // 发送 POST 请求
-        return restTemplate.postForEntity(requestUrl, requestEntity, String.class).getBody();
-
+        return new HttpEntity<>(encrypt, headers);
     }
 
 
